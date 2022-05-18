453608d79dcd1da3351d6001d898b9f69f8cfeef
João Vitor da Cruz Souza <99332002+JoaoVcz@users.noreply.github.com>
2022-05-18 18:01:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  {  StatusBar  }  from  'expo-status-bar' ;
+import  {  StyleSheet ,  Text ,  Image  ,  View  }  from  'react-native' ;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+importar  Img  de  './assets/Lua.png' ;
+
+exportar  função padrão  App ( ) {  
+  retornar  (
+    < Estilo de visualização  = { estilos . recipiente } >
+      < Estilo da imagem  = { estilos . img } fonte = { img } / > 
+
+    
+    < Estilo de visualização  = { estilos . caixatexto } > 
+    < Estilo de texto  = { estilos . titulo } > Lua < / Text >
+    < Estilo de texto  = { estilos . texto } > A lua é o satélite natural do < Text style = { styles . especial } > planeta Terra < / Text > , distanciados por aproximadamente 384.405km. < / Texto > 
+    < / Ver >
+    < / Ver >
+  ) ;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ estilos  const =  StyleSheet . criar ( {
+  recipiente : {
+    Flexão : 1 ,
+    backgroundColor : '#000' ,
+    alignItems : 'center' ,
+    justificarConteúdo : 'centro' ,
+    preenchimento : 30
+  } ,
+
+  img : {
+    largura : 100 ,
+    altura : 100 ,
+    margemBaixo : 50
+  } ,
+
+  caixatexto : {
+    borderColor : "#fff" ,
+    largura da borda : 2 ,
+    borderRadius : 20 ,
+    borderStyle : 'pontilhado' ,
+    preenchimentoVertical : 10 ,
+    preenchimentoHorizontal : 20
+  } ,
+
+  título : {
+    cor : '#fff' ,
+    tamanho da fonte : 30 ,
+    fontWeight : 'bold' ,
+    margemBottom : 20 ,
+    textAlign : 'centro'
+  } ,
+
+  texto : {
+    tamanho da fonte : 20 ,
+    cor : '#fff' ,
+    textAlign : 'centro'
+  } ,
+
+  especial : {
+    fontStyle : 'italic' ,
+    fontWeight : 'bold'
+  }
+} ) ;
+// feito por João Vitor Da Cruz Souza e Matheus Macedo
